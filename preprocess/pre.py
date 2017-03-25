1123fc92836c0b0ed3de5481a60ebfae21e1272d
ChangBae Bang <ChangBae Bang>
2017-03-25 22:48:03 +0900

    pre_test_8

--- preprocess/pre.py
+++ preprocess/pre.py
@@ -8,8 +8,8 @@ f = open('lung_genetic_mesh.txt')
 lines = f.readlines()
 for line in lines[:5]:
 	print line
-	name = name_reg.match(line)
-	pmid = pmid_reg.match(line)
+	name = name_reg.findall(line)
+	pmid = pmid_reg.findall(line)
 	print name
 	print pmid
 #sys.stdout.writelines(lines[:20])
