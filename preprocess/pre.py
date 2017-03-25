b9169b80a6661c42d8f598d66d6e9530678b2b70
ChangBae Bang <ChangBae Bang>
2017-03-25 22:47:02 +0900

    pre_test_8

--- preprocess/pre.py
+++ preprocess/pre.py
@@ -2,11 +2,14 @@ import tokenize
 import re
 import sys
 
-p = re.compile('(?<=\">)[^<]*(?=\<\/nameofsubstance)')
+name_reg = re.compile('(?<=\">)[^<]*(?=\<\/nameofsubstance)')
+pmid_reg = re.compile('(?<=\">)[^<]*(?=\<\/pmid)')
 f = open('lung_genetic_mesh.txt')
 lines = f.readlines()
 for line in lines[:5]:
 	print line
-	m = p.findall(line)
-	print m
+	name = name_reg.match(line)
+	pmid = pmid_reg.match(line)
+	print name
+	print pmid
 #sys.stdout.writelines(lines[:20])
