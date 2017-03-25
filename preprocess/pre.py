6314f7cb384c1521dfd38eb36da469b569b22478
ChangBae Bang <ChangBae Bang>
2017-03-25 22:04:24 +0900

    preprocess test

--- preprocess/pre.py
+++ preprocess/pre.py
@@ -1,6 +1,11 @@
 import tokenize
-
+import re
 import sys
+
+p = re.compile('>*<')
 f = open('lung_genetic_mesh.txt')
 lines = f.readlines()
-sys.stdout.writelines(lines[:20])
+for line in lines[:5]:
+	m = p.match(line)
+	print m
+#sys.stdout.writelines(lines[:20])
