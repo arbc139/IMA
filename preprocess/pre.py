c9ccf194dda626384e09c2da51c10ce2f0f15cdc
ChangBae Bang <ChangBae Bang>
2017-03-25 22:25:14 +0900

    pre_test_4

--- preprocess/pre.py
+++ preprocess/pre.py
@@ -6,6 +6,7 @@ p = re.compile('>[a-zA-Z0-9]+<')
 f = open('lung_genetic_mesh.txt')
 lines = f.readlines()
 for line in lines[:5]:
+	print line
 	m = p.match(line)
 	print m
 #sys.stdout.writelines(lines[:20])
