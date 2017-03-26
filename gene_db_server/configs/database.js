396960818d811ab2fdd0b1c05a5674f4323e6e8c
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-03-26 21:36:47 +0900

    Connect mysql with gene db server

---
+++ gene_db_server/configs/database.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: 'thwhd2017',
+  port: 3306,
+  database: 'mesh',
+};
--- gene_db_server/routes/index.js
+++ gene_db_server/routes/index.js
@@ -1,16 +1,28 @@
+
 const express = require('express');
+const mysql = require('mysql');
 const router = express.Router();
 
+const connection = mysql.createConnection(require('../configs/database'));
+
 // GET home page.
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', (request, response, next) => {
+  response.send('Home');
 });
 
 // GET SQL query results
 router.get('/sql', (request, response, next) => {
-  console.log('SQL query results');
+  console.log('request: ', request.body);
   // Find rows by SQL query in GENE DB.
-  response.send('SQL query results');
+  connection.query('SELECT * from Persons', (err, rows) => {
+    if (err) {
+      response.send('Invalid SQL statement');
+      return;
+    }
+
+    console.log('The solution is: ', rows);
+    response.send(rows);
+  });
 });
 
 module.exports = router;
