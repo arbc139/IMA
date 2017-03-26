76a90e70201234edec35e5df9553e9b1b471546b
ChangBae Bang <ChangBae Bang>
2017-03-26 23:08:11 +0900

    03-26.db_test.1

--- preprocess/pre.py
+++ preprocess/pre.py
@@ -1,17 +1,32 @@
 import tokenize
 import re
 import sys
-
+import pymysql
+ 
+# MySQL Connection 연결
+conn = pymysql.connect(host='localhost', user='root', password='',
+                       db='thwhd2017', charset='utf8')
+ 
+# Connection 으로부터 Cursor 생성
+curs = conn.cursor()
+pmid = 1234
+name = "abc"
+query = "INSERT INTO LUNG_SUBSTANCE (PM_ID, S_NAME) VALUES ('"+pmid+"', '"+name+"');"
+curs.execute(sql)
 name_reg = re.compile('(?<=\">)[^<]*(?=\<\/nameofsubstance)')
 pmid_reg = re.compile('(?<=\">)[^<]*(?=\<\/pmid)')
 f = open('lung_genetic_mesh.txt')
 lines = f.readlines()
-for line in lines[:5]:
-	print line
+for line in lines:
 	name = name_reg.findall(line)
 	pmid = pmid_reg.findall(line)
-	name_list = name[0].split(",")
-	print name
-	print name_list
-	print pmid
+	#query = "INSERT INTO LUNG_SUBSTANCE (PM_ID, S_NAME) VALUES ('"+pmid+"', '"+name+"');"
+	"""name_list = name[0].split(", ")
+	if(name_list[0] == "Antibodies" or name_list[0]=="Antigens"):
+		print name
+	if(name_list[0] != "Antibodies" and name_list[0]!="Antigens"):
+		if len(name_list) == 3:
+			print name"""
+
+		
 #sys.stdout.writelines(lines[:20])
