8e90c22df69dd540ece536d89f294e98c1f7a291
ChangBae Bang <ChangBae Bang>
2017-03-26 23:35:46 +0900

    03-26.db_test.12

--- preprocess/pre.py
+++ preprocess/pre.py
@@ -6,12 +6,7 @@ import pymysql
 conn = pymysql.connect(host='localhost', user='root', password='',db='mesh', charset='utf8')
  
 curs = conn.cursor()
-pmid = "1234"
-name = "abc"
-query = "INSERT INTO LUNG_SUBSTANCE (PM_ID, S_NAME) VALUES (%s, %s)"
-print query
-curs.execute(query,(pmid,name))
-conn.commit()
+
 name_reg = re.compile('(?<=\">)[^<]*(?=\<\/nameofsubstance)')
 pmid_reg = re.compile('(?<=\">)[^<]*(?=\<\/pmid)')
 f = open('lung_genetic_mesh.txt')
@@ -19,13 +14,14 @@ lines = f.readlines()
 for line in lines:
 	name = name_reg.findall(line)
 	pmid = pmid_reg.findall(line)
-	#query = "INSERT INTO LUNG_SUBSTANCE (PM_ID, S_NAME) VALUES ('"+pmid+"', '"+name+"');"
+	query = "INSERT INTO LUNG_SUBSTANCE (PM_ID, S_NAME) VALUES (%s, %s)"
+	curs.execute(query,(pmid,name))
+	conn.commit()
 	"""name_list = name[0].split(", ")
 	if(name_list[0] == "Antibodies" or name_list[0]=="Antigens"):
 		print name
 	if(name_list[0] != "Antibodies" and name_list[0]!="Antigens"):
 		if len(name_list) == 3:
 			print name"""
-
-		
+conn.close()	
 #sys.stdout.writelines(lines[:20])
