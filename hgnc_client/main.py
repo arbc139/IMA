3d439205ae0be8e8ad422ca3c0769292e5dbc780
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-03-30 17:47:42 +0900

    Fix tiny bug in HGNC client

--- hgnc_client/main.py
+++ hgnc_client/main.py
@@ -27,8 +27,6 @@ def get_max_score_doc(result_docs):
   return max(result_docs, key = lambda doc: doc['score'])
 
 for processed in all_processed:
-  if not test:
-    break
   # Hgnc response
   response = hgnc_http.request(
     '/search/' + processed['P_NAME'], 'GET', '', ''
