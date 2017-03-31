487e7ca5f718a7e214da333bf458614da7c25e67
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-03-31 12:09:59 +0900

    Remove print statement from HGNC client

--- hgnc_client/main.py
+++ hgnc_client/main.py
@@ -47,7 +47,6 @@ for processed in all_processed:
     sid_hgnc_map[processed['S_ID']] >= response['maxScore']):
     continue
 
-  print('docs:', response['docs'])
   sid_hgnc_map[processed['S_ID']] = response['maxScore']
   max_doc = get_max_score_doc(response['docs'])
   print('INSERT INTO LUNG_GENES (S_ID, PM_ID, HGNC_ID, SYMBOL, MAX_SCORE) VALUES (%s, %s, "%s", "%s", %s) ON DUPLICATE KEY UPDATE HGNC_ID="%s", SYMBOL="%s", MAX_SCORE=%s' % (
