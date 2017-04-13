b62b73b11f82fa2fc10a4f06efc5cb1ea59797a4
ChangBae Bang <ChangBae Bang>
2017-04-13 23:36:57 +0900

    prostate_start

---
+++ preprocess/pre_prostate.py
@@ -0,0 +1,27 @@
+import tokenize
+import re
+import sys
+import pymysql
+ 
+conn = pymysql.connect(host='localhost', user='root', password='',db='mesh', charset='utf8')
+ 
+curs = conn.cursor()
+
+name_reg = re.compile('(?<=\">)[^<]*(?=\<\/nameofsubstance)')
+pmid_reg = re.compile('(?<=\">)[^<]*(?=\<\/pmid)')
+f = open('lung_genetic_mesh.txt')
+lines = f.readlines()
+for line in lines:
+	name = name_reg.findall(line)
+	pmid = pmid_reg.findall(line)
+        query = "INSERT INTO PROSTATE_SUBSTANCE (PM_ID, S_NAME) VALUES (%s, %s)"
+	curs.execute(query,(pmid,name))
+	conn.commit()
+	"""name_list = name[0].split(", ")
+	if(name_list[0] == "Antibodies" or name_list[0]=="Antigens"):
+		print name
+	if(name_list[0] != "Antibodies" and name_list[0]!="Antigens"):
+		if len(name_list) == 3:
+			print name"""
+conn.close()	
+#sys.stdout.writelines(lines[:20])
