806a1a60a8fd6e4bfc397d2ff9a3114ce4a39649
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-05-03 00:02:21 +0900

    Fix tiny bug on 'fulfill_is_family'

--- mesh_family_processing/fulfill_is_family/lung/main.py
+++ mesh_family_processing/fulfill_is_family/lung/main.py
@@ -4,7 +4,6 @@ from urllib.parse import quote
 import difflib
 import math
 import pymysql
-import re
 import sys
 import time
 
@@ -31,23 +30,22 @@ print('Find all genes time:', get_elapsed_seconds(get_current_millis(), elapsed_
 # Find with checking family
 for gene in all_genes:
   elapsed_millis = get_current_millis()
-  mesh_term = re.sub('\'', '', gene['MESH_TERM'])
-  print('Mesh Term:', mesh_term)
+  print('Mesh Term:', gene['MESH_TERM'])
   qualifiers = None
   with db.cursor(pymysql.cursors.DictCursor) as cursor:
-    cursor.execute('SELECT * FROM MESH_QUALIFIER where NAME = %s', (mesh_term,))
+    cursor.execute('SELECT * FROM MESH_QUALIFIER where NAME = %s', (gene['MESH_TERM'],))
     qualifiers = cursor.fetchall()
   print('Qualifiers:', qualifiers)
   print('Find qualifiers time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
   descriptors = None
   with db.cursor(pymysql.cursors.DictCursor) as cursor:
-    cursor.execute('SELECT * FROM MESH_DESCRIPTOR where NAME = %s', (mesh_term,))
-    processeds = cursor.fetchall()
+    cursor.execute('SELECT * FROM MESH_DESCRIPTOR where NAME = %s', (gene['MESH_TERM'],))
+    descriptors = cursor.fetchall()
   print('Descriptors:', descriptors)
   print('Find descriptors time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
   supplementals = None
   with db.cursor(pymysql.cursors.DictCursor) as cursor:
-    cursor.execute('SELECT * FROM MESH_SUPPLEMENTAL where NAME = %s', (mesh_term,))
+    cursor.execute('SELECT * FROM MESH_SUPPLEMENTAL where NAME = %s', (gene['MESH_TERM'],))
     supplementals = cursor.fetchall()
   print('Supplementals:', supplementals)
   print('Find supplementals time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
