3d4069b958b0a6525dece5c31b9b7f7c4b39368a
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-05-02 22:53:02 +0900

    Complement MESH_DESCRIPTOR make script

---
+++ mesh_family_processing/make_mesh_db/descripter/main.py
@@ -0,0 +1,41 @@
+
+import xml.etree.ElementTree as et
+import pymysql
+import time
+
+get_current_millis = lambda: int(round(time.time() * 1000))
+def get_elapsed_seconds(current_time, elapsed_millis):
+  return (current_time - elapsed_millis) / 1000.0
+
+db = pymysql.connect(host='localhost', user='root', password='', db='mesh', charset='utf8')
+
+elapsed_millis = get_current_millis()
+root = et.parse('../input/desc2017.xml').getroot()
+print('Parse xml time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
+
+elapsed_millis = get_current_millis()
+records = root.findall('DescriptorRecord')
+print('Find all records time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
+
+rows = []
+for record in records:
+  uid = record.findtext('DescriptorUI')
+  name = record.find('DescriptorName').findtext('String')
+  tree_numbers = []
+  for tree_number_record in record.findall('TreeNumberList'):
+    tree_numbers.append(tree_number_record.findtext('TreeNumber'))
+  
+  print('INSERT INTO MESH_DESCRIPTOR (UID, NAME, TREE_NUMBERS) VALUES (%s, "%s", "%s")' % (
+    uid, name, str(tree_numbers), name, str(tree_numbers)))
+  
+  rows.append([uid, name, tree_numbers])
+
+elapsed_millis = get_current_millis()
+# Send a query to insert MESH_DESCRIPTOR.
+with db.cursor(pymysql.cursors.DictCursor) as cursor:
+  cursor.executemany(
+    'INSERT INTO MESH_DESCRIPTOR (UID, NAME, TREE_NUMBERS) VALUES (%s, "%s", "%s")',
+    rows)
+  )
+db.commit()
+print('Insert mesh db time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
