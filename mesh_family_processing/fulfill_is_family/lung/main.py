3a92b5fd393d4b628d33bf7972b9c40e1a46352f
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-05-03 00:33:19 +0900

    Complete 'fulfill_is_family'

--- mesh_family_processing/fulfill_is_family/lung/main.py
+++ mesh_family_processing/fulfill_is_family/lung/main.py
@@ -2,6 +2,7 @@
 
 from urllib.parse import quote
 import difflib
+import json
 import math
 import pymysql
 import sys
@@ -28,6 +29,7 @@ with db.cursor(pymysql.cursors.DictCursor) as cursor:
 print('Find all genes time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
 
 # Find with checking family
+values = []
 for gene in all_genes:
   elapsed_millis = get_current_millis()
   print('Mesh Term:', gene['MESH_TERM'])
@@ -43,24 +45,48 @@ for gene in all_genes:
     descriptor = cursor.fetchone()
   print('Descriptor:', descriptor)
   print('Find descriptor time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
-  supplemental = None
-  with db.cursor(pymysql.cursors.DictCursor) as cursor:
-    cursor.execute('SELECT * FROM MESH_SUPPLEMENTAL where NAME = %s', (gene['MESH_TERM'],))
-    supplemental = cursor.fetchone()
-  print('Supplemental:', supplemental)
-  print('Find supplemental time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
-  break
-  """
-  print('UPDATE LUNG_GENES SET MESH_TERM="%s" WHERE S_ID=%s' % (
-    full_mesh_term, gene['S_ID']))
+  
+  tree_numbers = None
+  if qualifier:
+    tree_numbers = json.loads(qualifier['TREE_NUMBERS'])
+  elif descriptor:
+    tree_numbers = json.loads(descriptor['TREE_NUMBERS'])
+  else:
+    print('There is no mesh term for qualifier and descriptor')
+    continue
+  
+  print('Tree numbers:', tree_numbers)
+  
+  is_family = False
+  for tree_number in tree_numbers:
+    all_qualifiers = None
+    with db.cursor(pymysql.cursors.DictCursor) as cursor:
+      cursor.execute('SELECT * FROM MESH_QUALIFIER where TREE_NUMBERS REGEXP ".*%s\..*"' % (tree_number))
+      all_qualifiers = cursor.fetchall()
+    all_descriptors = None
+    with db.cursor(pymysql.cursors.DictCursor) as cursor:
+      cursor.execute('SELECT * FROM MESH_DESCRIPTOR where TREE_NUMBERS REGEXP ".*%s\..*"' % (tree_number))
+      all_descriptors = cursor.fetchall()
+    all_supplementals = None
+    with db.cursor(pymysql.cursors.DictCursor) as cursor:
+      cursor.execute('SELECT * FROM MESH_SUPPLEMENTAL where TREE_NUMBERS REGEXP ".*%s\..*"' % (tree_number))
+      all_supplementals = cursor.fetchall()
+    if len(all_qualifiers) == 0 and len(all_descriptors) == 0 and len(all_supplementals) == 0:
+      is_family = True
+      break
 
-  elapsed_millis = get_current_millis()
-  # Send a query to update LUNG_GENES.
-  with db.cursor(pymysql.cursors.DictCursor) as cursor:
-    cursor.execute(
-      'UPDATE LUNG_GENES SET MESH_TERM="%s" WHERE S_ID=%s',
-      (full_mesh_term, gene['S_ID'])
-    )
-  db.commit()
-  print('Update gene time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
-  """
+  print('UPDATE LUNG_GENES SET IS_FAMILY=%d WHERE S_ID=%s' % (
+    1 if is_family else 0, gene['S_ID']))
+  values.append([1 if is_family else 0, gene['S_ID']])
+
+"""
+elapsed_millis = get_current_millis()
+# Send a query to update LUNG_GENES.
+with db.cursor(pymysql.cursors.DictCursor) as cursor:
+  cursor.executemany(
+    'UPDATE LUNG_GENES SET IS_FAMILY=%d WHERE S_ID=%s',
+    values
+  )
+db.commit()
+print('Update gene time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
+"""
