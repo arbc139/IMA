188ca7b021b9deb7e6a724276af35ec4e624718f
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-05-02 21:17:31 +0900

    Introduce find full mesh term

---
+++ mesh_family_processing/find_full_mesh_term/lung/http_wrapper.py
@@ -0,0 +1,33 @@
+
+#########################################################
+# This is just a copy of ../hgnc_client/http_wrapper.py #
+#########################################################
+import json
+import httplib2 as http
+import time
+# For Python 3
+from urllib.parse import urlparse, urlencode
+
+class HttpWrapper():
+  
+  def __init__(self, uri):
+    self.uri = uri
+  
+  def request(self, path, method, query, body):
+    while True:
+      response, content = http.Http().request(
+        urlparse(self.uri + path + '?' + urlencode(query)).geturl(), # URL
+        method,
+        body,
+        { 'Accept': 'application/json' } # Header
+      )
+
+      if response['status'] != '200':
+        print('Request failed:', response['status'])
+        # TODO(totoro): Find workaround better than this...
+        time.sleep(5)
+        print('After 5 seconds, try agian')
+        continue
+      else:
+        return json.loads(content)
+    
---
+++ mesh_family_processing/find_full_mesh_term/lung/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+from urllib.parse import quote
+import difflib
+import math
+import pymysql
+import sys
+import time
+
+get_current_millis = lambda: int(round(time.time() * 1000))
+def get_elapsed_seconds(current_time, elapsed_millis):
+  return (current_time - elapsed_millis) / 1000.0
+
+START_S_ID = None
+if len(sys.argv) > 1:
+  START_S_ID = sys.argv[1]
+
+db = pymysql.connect(host='localhost', user='root', password='', db='mesh', charset='utf8')
+
+elapsed_millis = get_current_millis()
+# Get all genes in DB.
+all_genes = None
+with db.cursor(pymysql.cursors.DictCursor) as cursor:
+  query = 'SELECT * FROM LUNG_GENES ORDER BY S_ID' if START_S_ID is None \
+    else 'SELECT * FROM LUNG_GENES WHERE S_ID > %s ORDER BY S_ID' % (START_S_ID)
+  cursor.execute(query)
+  all_genes = cursor.fetchall()
+print('Find all genes time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
+
+# Find full mesh term
+for gene in all_genes:
+  elapsed_millis = get_current_millis()
+  processeds = None
+  with db.cursor(pymysql.cursors.DictCursor) as cursor:
+    cursor.execute('SELECT * FROM LUNG_PROCESSED where S_ID = %s', (gene['S_ID'],))
+    processeds = cursor.fetchall()
+  print('Find processeds time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
+  
+  full_mesh_term = max(processeds, lambda processed: len(processed['P_NAME']))  
+  print(full_mesh_term)
+  break
