35b548a58f22c149bde1299c4be5b315ac72500c
김도영 <gimdoyeong@gimdoyeong-ui-MacBook-Pro.local>
2017-05-02 14:33:25 +0900

    Introduce process family names

---
+++ process_family_name/lung/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import difflib
+import math
+import pymysql
+import sys
+import time
+
+get_current_millis = lambda: int(round(time.time() * 1000))
+def get_elapsed_seconds(current_time, elapsed_millis):
+  return (current_time - elapsed_millis) / 1000.0
+
+START_S_ID = None
+if len(sys.argv) > 1:
+  START_S_ID = sys.argv[1]
+
+db = pymysql.connect(host='localhost', user='root', password='', db='mesh', charset='utf8')
+
+elapsed_millis = get_current_millis()
+# Get all genes in DB.
+all_genes = None
+with db.cursor(pymysql.cursors.DictCursor) as cursor:
+  query = 'SELECT * FROM LUNG_GENES ORDER BY S_ID' if START_S_ID is None \
+    else 'SELECT * FROM LUNG_GENES WHERE S_ID > %s ORDER BY S_ID' % (START_S_ID)
+  cursor.execute(query)
+  all_genes = cursor.fetchall()
+print('Find all genes time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
+
+for gene in all_genes:
+  if not gene['HGNC_FAMILY_NAME'] or '|' in gene['HGNC_FAMILY_NAME']:
+    continue
+  
+  family_names = gene['HGNC_FAMILY_NAME'].split('|')
+  print(family_names)
+  break
+    
+  """
+  # Get a name similarity score between MeSH query and HGNC family name.
+  name_score = difflib.SequenceMatcher(None, processed['P_NAME'].lower(), gene_family_info['GENE_FAMILY_NAME'].lower()).ratio()
+
+  print('UPDATE LUNG_GENES SET MESH_NAME="%s", HGNC_FAMILY_NAME="%s", NAME_SCORE=%s WHERE S_ID=%s' % (
+    processed['P_NAME'], gene_family_info['GENE_FAMILY_NAME'], name_score, processed['S_ID']))
+
+  elapsed_millis = get_current_millis()
+  # Send a query to update LUNG_GENES.
+  with db.cursor(pymysql.cursors.DictCursor) as cursor:
+    cursor.execute(
+      'UPDATE LUNG_GENES SET MESH_NAME="%s", HGNC_FAMILY_NAME="%s", NAME_SCORE=%s WHERE S_ID=%s',
+      (processed['P_NAME'], gene_family_info['GENE_FAMILY_NAME'], name_score, processed['S_ID'])
+    )
+  db.commit()
+  print('Update gene time:', get_elapsed_seconds(get_current_millis(), elapsed_millis))
+  """
+
