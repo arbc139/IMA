78c31263d67e604bf1297b45aa8e600104b3615b
Totoro <arbc139@naver.com>
2017-05-03 20:44:09 +0900

    Make weka analysis more general

--- weka_analysis/main.py
+++ weka_analysis/main.py
@@ -2,10 +2,17 @@
 from csv_manager import CsvManager
 from weka_manager import WekaManager
 from weka_parser import WekaParser
+import sys
+
+if len(sys.argv) < 3:
+  raise RuntimeError('Need to set input file and output file')
+
+INPUT_FILE = sys.argv[1]
+OUTPUT_FILE = sys.argv[2]
 
 parser = None
 weka_objects = None
-with open('input/lung_gene_result.txt', 'r') as weka_file:
+with open(INPUT_FILE, 'r') as weka_file:
   parser = WekaParser(weka_file)
   weka_objects = parser.parse()
 
@@ -14,7 +21,7 @@ weka_manager.normalize()
 
 analyze_result = weka_manager.analyze()
 
-with open('output/lung_gene_relationship.csv', 'w') as csv_file:
+with open(OUTPUT_FILE, 'w') as csv_file:
   csv_manager = CsvManager(csv_file, ['First', 'Second', 'Score'])
   for relationship, score in analyze_result.items():
     csv_manager.write_row({
