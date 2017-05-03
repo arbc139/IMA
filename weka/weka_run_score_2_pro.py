cfb9933a584ad8e0dd900869d8acb8b93f370d87
ChangBae Bang <ChangBae Bang>
2017-05-03 15:01:11 +0900

    1.5_2

---
+++ weka/weka_run_score_2_pro.py
@@ -0,0 +1,41 @@
+import tokenize
+import re
+import sys
+import pymysql
+
+f_2 = open('prostate_gene_2.arff','w')
+
+conn = pymysql.connect(host='localhost', user='root', password='',db='mesh', charset='utf8')
+ 
+curs = conn.cursor()
+query = "SELECT  DISTINCT SYMBOL FROM PROSTATE_GENES WHERE MAX_SCORE > 2 "
+curs.execute(query)
+symbols = curs.fetchall()
+query = "SELECT  DISTINCT PM_ID FROM PROSTATE_GENES"
+curs.execute(query)
+pm_ids = curs.fetchall()
+f_2.write('@relation PROSTATE_CANCER.symbolic'+'\n\n')
+for sym in symbols:
+	f_2.write('@attribute '+sym[0]+' {0,1}\n')
+f_2.write('@data\n')
+
+thesis = []
+for pm_id in pm_ids:
+	del thesis[:] 
+	query = "SELECT  SYMBOL FROM PROSTATE_GENES WHERE  IS_FAMILY = 0 AND PM_ID="+str(pm_id[0])+" AND MAX_SCORE > 2;" 
+	curs.execute(query)
+	match = curs.fetchall()
+	#print match
+	for sym in symbols:
+		if sym in match:
+			#print sym
+			#print match
+			thesis.append('1')
+		else:
+			thesis.append('?')
+	outstr = ', '.join(thesis)
+	#print outstr
+	f_2.write(outstr+'\n')
+conn.close()	
+
+
