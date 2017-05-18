7bcc0c828803e8ff787a654d6bb62702a2bc0fda
Totoro <arbc139@naver.com>
2017-05-18 15:34:05 +0900

    Change parameter parser smarter in weka_analysis

--- weka_analysis/main.py
+++ weka_analysis/main.py
@@ -4,15 +4,20 @@ from weka_manager import WekaManager
 from weka_parser import WekaParser
 import sys
 
-if len(sys.argv) < 3:
-  raise RuntimeError('Need to set input file and output file')
+def parse_commands(argv):
+  from optparse import OptionParser
+  parser = OptionParser('"')
+  parser.add_option('-i', '--inputFile', dest='input_file')
+  parser.add_option('-o', '--outputFile', dest='output_file')
+  
+  options, otherjunk = parser.parse_args(argv)
+  return options
 
-INPUT_FILE = sys.argv[1]
-OUTPUT_FILE = sys.argv[2]
+options = parse_commands(sys.argv[1:])
 
 parser = None
 weka_objects = None
-with open(INPUT_FILE, 'r') as weka_file:
+with open(options.input_file, 'r') as weka_file:
   parser = WekaParser(weka_file)
   weka_objects = parser.parse()
 
@@ -22,7 +27,7 @@ weka_manager.normalize()
 
 analyze_result = weka_manager.analyze()
 
-with open(OUTPUT_FILE, 'w+') as csv_file:
+with open(options.output_file, 'w+') as csv_file:
   csv_manager = CsvManager(csv_file, ['Type', 'Source', 'Target', 'Weight'])
   for relationship, weight in analyze_result.items():
     csv_manager.write_row({
