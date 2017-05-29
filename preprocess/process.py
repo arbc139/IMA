bcc2d626e20ad0abf63dd3bd20332f0ba06a16d9
ChangBae Bang <ChangBae Bang>
2017-05-29 22:09:34 +0900

    rna and long 23

--- preprocess/process.py
+++ preprocess/process.py
@@ -1,7 +1,9 @@
 import tokenize
 import re
 import pymysql
- 
+import sys
+disease = sys.argv[1]
+
 conn = pymysql.connect(host='localhost', user='root', password='',db='mesh', charset='utf8') 
 curs = conn.cursor()
 type_reg = re.compile('Type \w+|type \w+|subtype \w+|sub\-type \w+|Subtype \w+|Sub\-type \w+')
@@ -11,8 +13,17 @@ spcae_reg = re.compile('[ ]\w+')
 protein_reg = re.compile('\w*[ ]protein')
 protein_reg_2 = re.compile('\w*[ ]protein$')
 Protein_reg = re.compile('\w*[ ]Protein$')
+longnon_reg_1 = re.compile('\w*long[ ]non-coding[ ]RNA\w*')
+longnon_2 = re.compile('\w*long[ ]noncoding[ ]RNA\w*')
+longnon_3 = re.compile('\w*long[ ]non coding[ ]RNA\w*')
+longnon_4 = re.compile('\w*non[ ]coding[ ]RNA\w*')
+longnon_5 = re.compile('\w*noncoding[ ]RNA\w*')
+longnon_6 = re.compile('\w*non-coding[ ]RNA\w*')
+
 test_cases = [['1','Antibodies, Monoclonal, Humanized'],['2','Chorionic Gonadotropin, beta Subunit, Human'],['3','Receptors, Pituitary Adenylate Cyclase-Activating Polypeptide, Type III'],['4','Receptors, Tumor Necrosis Factor, Member 10c'],['5','ATP Binding Cassette Transporter, Sub-Family G, Member 2'],['6','ABC protein, Human'],['7','Biomarkers, Tumor'],['8','Receptor, Epidermal Growth Factor'],['9', 'MicroRNA']]
 substance_replace = []
+
+
 
 def receptor(name):
 	global substance_replace
@@ -37,6 +48,18 @@ def receptor(name):
 	else:
 		temp_sub = ' '.join(name) 
 	substance_replace.append(temp_sub)
+def micro_delete(name):
+	gene_area = name[0].split(" ")
+	substance_replace.append(gene_area[0])
+def long_delete(name):
+	gene_area = name[0].split(" ")
+	gene = re.sub('long[ ]non-coding[ ]RNA', '', gene_area[0])
+	gene = re.sub('long[ ]noncoding[ ]RNA', '', gene)
+	gene = re.sub('long[ ]non[ ]coding[ ]RNA', '', gene)
+	gene = re.sub('non[ ]coding[ ]RNA', '', gene)
+	gene = re.sub('noncoding[ ]RNA', '', gene)
+	gene = re.sub('non-coding[ ]RNA', '', gene)
+	substance_replace.append(gene)
 
 def anti(name):
 	temp_sub = ' '.join(name)	
@@ -57,8 +80,7 @@ def others(name):
 	
 
 if __name__ == '__main__':
-	query = "SELECT * FROM PERIO_SUBSTANCE"
-	curs.execute(query)
+	query = "SELECT * FROM "+disease+"_SUBSTANCE"	curs.execute(query)
 	rows = curs.fetchall()
 	for row in rows :
 		del substance_replace[:] 
@@ -66,8 +88,21 @@ if __name__ == '__main__':
 		protein_detect = protein_reg.findall(name[0])
 		protein_detect_2 = protein_reg_2.findall(name[0])
 		Protein_detect = Protein_reg.findall(name[0])
+		micro_detect= micro_reg.findall(name[0])
+		longnon_detect = longnon_reg_1.findall(name[0])
+		longnon_detect_2 = longnon_2.findall(name[0])
+		longnon_detect_3 = longnon_3.findall(name[0])
+		longnon_detect_4 = longnon_4.findall(name[0])
+		longnon_detect_5 = longnon_5.findall(name[0])
+		longnon_detect_6 = longnon_6.findall(name[0])
+		
+
 		if protein_detect or Protein_detect or protein_detect_2:
 			protein_delete(name)
+		elif micro_detect:
+			micro_delete(name)
+		elif longnon_detect or longnon_detect_2 or longnon_detect_3  or longnon_detect_4 or longnon_detect_5 or longnon_detect_6:
+			long_delete(name)
 		else:
 			if name[0] == 'Receptor' or name[0] == 'Receptors':
 				receptor(name)
@@ -76,6 +111,6 @@ if __name__ == '__main__':
 			else:
 				others(name)
 		for elm in substance_replace:
-			query = "INSERT INTO PERIO_PROCESSED (S_ID, PM_ID,P_NAME) VALUES (%s, %s,%s)"	
+			query = "INSERT INTO "+disease+"_PROCESSED (S_ID, PM_ID,P_NAME) VALUES (%s, %s,%s)"	
 			curs.execute(query,(row[0],row[1],elm))
 			conn.commit()
