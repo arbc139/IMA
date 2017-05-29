0f8acf97511050fc238498e14a4800d514b69a4e
ChangBae Bang <ChangBae Bang>
2017-05-29 21:30:24 +0900

    rna and long 11

--- preprocess/add_process.py
+++ preprocess/add_process.py
@@ -10,17 +10,20 @@ curs = conn.cursor()
 type_reg = re.compile('Type \w+|type \w+|subtype \w+|sub\-type \w+|Subtype \w+|Sub\-type \w+')
 name_reg = re.compile('(?<=\">)[^<]*(?=\<\/nameofsubstance)')
 
-spcae_reg = re.compile('[ ]\w+')
-protein_reg = re.compile('\w*[ ]protein')
-protein_reg_2 = re.compile('\w*[ ]protein$')
-Protein_reg = re.compile('\w*[ ]Protein$')
-micro_reg = re.compile('\w*[ ]microRNA\w*')
 longnon = re.complie('\w*long[ ]non-coding[ ]RNA\w*')
 longnon_2 = re.complie('\w*long[ ]noncoding[ ]RNA\w*')
 longnon_3 = re.complie('\w*long[ ]non coding[ ]RNA\w*')
 longnon_4 = re.complie('\w*non[ ]coding[ ]RNA\w*')
 longnon_5 = re.complie('\w*noncoding[ ]RNA\w*')
 longnon_6 = re.complie('\w*non-coding[ ]RNA\w*')
+
+
+spcae_reg = re.compile('[ ]\w+')
+protein_reg = re.compile('\w*[ ]protein')
+protein_reg_2 = re.compile('\w*[ ]protein$')
+Protein_reg = re.compile('\w*[ ]Protein$')
+micro_reg = re.compile('\w*[ ]microRNA\w*')
+
 
 
 substance_replace = []
